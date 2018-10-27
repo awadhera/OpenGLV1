42bcb9b1c048e2c666b21d9e449f3c393c8ba805
AnshumanW <AnshumanW@adherium.com>
2018-10-28 01:10:05 +1300

    First commit

---
+++ TestGL.cpp
@@ -0,0 +1,44 @@
+#include <GL/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main()
+{
+	if(!glfwInit())
+	{
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return 1;
+	}
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", nullptr, nullptr);
+	if(!window)
+	{
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	glewInit();
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* version = glGetString(GL_VERSION);
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	GLfloat points[] = 	{ 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	glfwTerminate();
+	return 0;
+}
